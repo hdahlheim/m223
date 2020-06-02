cce3de696df44bff0aeadfa0c6ac7c1c2943c69c
Henning Dahlheim <hactar@cyberkraft.ch>
2020-06-02 12:39:41 +0200

    Update games context with public api

--- lib/gsgms/games.ex
+++ lib/gsgms/games.ex
@@ -1,5 +1,7 @@
 defmodule GSGMS.Games do
   alias __MODULE__.Players
 
+  defdelegate get_players(), to: Players, as: :list_players
   defdelegate get_player_by_id(id), to: Players, as: :get_player!
+  defdelegate create_player(player_info), to: Players
 end
