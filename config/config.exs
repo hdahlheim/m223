b287e47f7a888d03967c629b6c3a63c5787c0c54
Henning Dahlheim <hactar@cyberkraft.ch>
2020-06-02 20:53:34 +0200

    Add timezone database Tzdata

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,8 @@
 # General application configuration
 use Mix.Config
 
+config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
+
 config :gsgms,
   namespace: GSGMS,
   ecto_repos: [GSGMS.Repo],
--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule GSGMS.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.4.0", only: :dev}
+      {:credo, "~> 1.4.0", only: :dev},
+      {:tzdata, "~> 1.0.3"}
     ]
   end
 
